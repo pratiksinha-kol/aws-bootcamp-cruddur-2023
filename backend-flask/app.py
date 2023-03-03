f9e9f07609e7d6462f5c8d5ac2100424e0f70411
pratiksinha-kol <pratik.proriters@gmail.com>
2023-03-03 15:42:08 +0000

    X-Ray Activated again

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -57,8 +57,8 @@ provider.add_span_processor(processor)
 
 
 # X-RAY ----------
-# xray_url = os.getenv("AWS_XRAY_URL")
-# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 # Show this in the logs within the backend-flask app (STDOUT)
 simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
@@ -70,7 +70,7 @@ tracer = trace.get_tracer(__name__)
 app = Flask(__name__)
 
 # X-RAY ----------
-# XRayMiddleware(app, xray_recorder)
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb ---------
 # Initialize automatic instrumentation with Flask
