5b8e8a4975e2aa20b014fd48e9bf9171b55ad92e
pratiksinha-kol <pratik.proriters@gmail.com>
2023-03-05 14:54:09 +0000

    Integrate Signin-Signup, Confirmation & Docker env

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -1,10 +1,12 @@
 import './SignupPage.css';
 import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
+
 
 export default function SignupPage() {
 
@@ -16,15 +18,39 @@ export default function SignupPage() {
   const [errors, setErrors] = React.useState('');
 
   const onsubmit = async (event) => {
-    event.preventDefault();
-    console.log('SignupPage.onsubmit')
+    //  event.preventDefault();
+    //  console.log('SignupPage.onsubmit')
     // [TODO] Authenication
-    Cookies.set('user.name', name)
-    Cookies.set('user.username', username)
-    Cookies.set('user.email', email)
-    Cookies.set('user.password', password)
-    Cookies.set('user.confirmation_code',1234)
-    window.location.href = `/confirm?email=${email}`
+    //  Cookies.set('user.name', name)
+    //  Cookies.set('user.username', username)
+    //  Cookies.set('user.email', email)
+    //  Cookies.set('user.password', password)
+    //  Cookies.set('user.confirmation_code',1234)
+    //  window.location.href = `/confirm?email=${email}`
+    //  return false
+    //}
+
+    event.preventDefault();
+    setErrors('')
+    try {
+      const { user } = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          name: name,
+          email: email,
+          preferred_username: username,
+        },
+        autoSignIn: { // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        }
+      });
+      console.log(user);
+      window.location.href = `/confirm?email=${email}`
+    } catch (error) {
+      console.log(error);
+      setErrors(error.message)
+    }
     return false
   }
 
@@ -42,7 +68,7 @@ export default function SignupPage() {
   }
 
   let el_errors;
-  if (errors){
+  if (errors) {
     el_errors = <div className='errors'>{errors}</div>;
   }
 
@@ -52,7 +78,7 @@ export default function SignupPage() {
         <Logo className='logo' />
       </div>
       <div className='signup-wrapper'>
-        <form 
+        <form
           className='signup_form'
           onSubmit={onsubmit}
         >
@@ -63,7 +89,7 @@ export default function SignupPage() {
               <input
                 type="text"
                 value={name}
-                onChange={name_onchange} 
+                onChange={name_onchange}
               />
             </div>
 
@@ -72,7 +98,7 @@ export default function SignupPage() {
               <input
                 type="text"
                 value={email}
-                onChange={email_onchange} 
+                onChange={email_onchange}
               />
             </div>
 
@@ -81,7 +107,7 @@ export default function SignupPage() {
               <input
                 type="text"
                 value={username}
-                onChange={username_onchange} 
+                onChange={username_onchange}
               />
             </div>
 
@@ -90,7 +116,7 @@ export default function SignupPage() {
               <input
                 type="password"
                 value={password}
-                onChange={password_onchange} 
+                onChange={password_onchange}
               />
             </div>
           </div>
