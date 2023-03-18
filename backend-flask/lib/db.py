e2b29bc9416a550a835cfb6a221f09b202f08bd2
pratiksinha-kol <pratik.proriters@gmail.com>
2023-03-18 03:51:21 +0000

    Params

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -39,7 +39,7 @@ class Db:
       try:
         with self.pool.connection() as conn:
             cur = conn.cursor()
-            cur.execute(sql, *params)
+            cur.execute(sql, params)
             if is_returning_id:
                 returning_id = cur.fetchone()[0]
             conn.commit()
