7d3eb4d3f2e39dc6b1e53966981909d20b07ab42
pratiksinha-kol <pratik.proriters@gmail.com>
2023-03-21 07:37:47 +0000

    Backend not running

--- backend-flask/lib/cognito_jwt_token.py
+++ backend-flask/lib/cognito_jwt_token.py
@@ -1,4 +1,4 @@
-HTTP_HEADER = "Authorization"
+#HTTP_HEADER = "Authorization"
 
 import time
 import requests
@@ -16,7 +16,7 @@ class TokenVerifyError(Exception):
 
 def extract_access_token(request_headers):
     access_token = None
-    auth_header = request_headers.get(HTTP_HEADER)
+    auth_header = request_headers.get("Authorization")
     if auth_header and " " in auth_header:
         _, access_token = auth_header.split()
     return access_token
