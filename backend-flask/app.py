e5ab7726be14f4ba49f17f58ed21ac2c1377e21b
Pratik Sinha <pratik.proriters+github@gmail.com>
2023-03-25 16:11:59 +0000

    Implement change

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 import os
 import sys
 
+from services.users_short import *
 from services.home_activities import *
 from services.user_activities import *
 from services.create_activity import *
@@ -313,6 +314,10 @@ def data_activities_reply(activity_uuid):
         return model['data'], 200
     return
 
+@app.route("/api/users/@<string:handle>/short", methods=['GET'])
+def data_users_short(handle):
+  data = UsersShort.run(handle)
+  return data, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,13 +3,11 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'sample@test.com', 'andrewbrown' ,'MOCK'),
   ('PRATIK SINHA', 'pratik.sinha08+zxc@gmail.com', 'pratiksinha' ,'MOCK'),
-  ('Andrew Bayko', 'example@gmail.com', 'bayko' ,'MOCK');
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     (SELECT uuid from public.users WHERE users.handle = 'pratiksinha' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
