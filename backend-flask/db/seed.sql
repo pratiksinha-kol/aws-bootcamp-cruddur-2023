6356bea7bc55f4bdc6b23c4a71cc05410a98d88c
pratiksinha-ans <127577107+pratiksinha-ans@users.noreply.github.com>
2023-03-26 18:54:50 +0530

    Update seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'sample@test.com', 'andrewbrown' ,'MOCK'),
   ('PRATIK SINHA', 'pratik.sinha08+zxc@gmail.com', 'pratiksinha' ,'MOCK'),
+  ('Andrew Brown', 'sample@test.com', 'andrewbrown' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
