6b8e61e700eee818c291f4fb9b169865ab57fd97
pratiksinha-cruddur <pratik.proriters+cruddur@gmail.com>
2023-04-12 13:30:52 +0000

    Started Implementing Users Profile Page

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,32 +1,19 @@
-from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
     try:
-    # X-ray ---->
-    #  segment = xray_recorder.begin_segment('user-activities')
-    # above line not required anymore (refer line 32, https://docs.aws.amazon.com/xray-sdk-for-python/latest/reference/basic.html)
-
       model = {
-      'errors': None,
-      'data': None
-    }
-
-      now = datetime.now(timezone.utc).astimezone()
-
+        'errors': None,
+        'data': None
+      }
+    
       if user_handle == None or len(user_handle) < 1:
         model['errors'] = ['blank_user_handle']
       else:
-        now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
+        sql = db.template('users','show')
+        results = db.query_array_json(sql)            
+        model['data'] = results
     
     # X-ray ---->
       subsegment = xray_recorder.begin_subsegment('mock-data-subsegment')
