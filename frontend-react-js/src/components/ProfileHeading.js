2d4eeef817166d47ab0e9cb4061581198bf7ed5d
pratiksinha-cruddur <pratik.proriters+cruddur@gmail.com>
2023-04-16 13:52:00 +0000

    Banner implementation

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -2,14 +2,22 @@ import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
 export default function ProfileHeading(props) {
+  const backgroundImages = 'url("https://assets.pratiksinha.link/banners/banner1.jpg")';
+  const styles = {
+    backgroundImages: backgroundImages,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
   return (
     <div className='activity_feed_heading profile_heading'>
           <div className='title'>{props.profile.display_name}</div>
           <div className="cruds_count">{props.profile.cruds_count}Cruds</div>
-
-          <div className='avatar'>
-            <img src='https://assets.pratiksinha.link/avatars/data.jpg'></img>
+          <div className="banner" style={styles}>
+            <div className='avatar'>
+              <img src='https://assets.pratiksinha.link/avatars/data.jpg'></img>
+            </div>
           </div>
+          
           <div className='display_name'>{props.display_name}</div>
           <div className='handle'>{props.handle}</div>
           <EditProfileButton setPopped={props.setPopped}/>
