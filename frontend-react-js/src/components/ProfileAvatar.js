ed7df4636faca41ccb1804344ba0c069723fb41f
pratiksinha-kol <pratik.proriters@gmail.com>
2023-05-06 06:27:16 +0000

    Rendering Avatar from CloudFront completed

---
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -0,0 +1,18 @@
+import './ProfileAvatar.css';
+
+export default function ProfileAvatar(props) {
+  //const backgroundImage = `url("https://assets.cruddur.com/avatars/${props.id}.jpg")`;
+  const backgroundImage = `url("https://assets.pratiksinha.link/avatars/${props.id}.jpg")`;
+  const styles = {
+    backgroundImage: backgroundImage,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
+  return (
+    <div 
+      className="profile-avatar"
+      style={styles}
+    ></div>
+  );
+}
--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,6 +1,9 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
+import ProfileAvatar from 'components/ProfileAvatar'
+
+
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.pratiksinha.link/banners/banner1.jpg")';
   const styles = {
@@ -13,10 +16,8 @@ export default function ProfileHeading(props) {
           <div className='title'>{props.profile.display_name}</div>
           <div className="cruds_count">{props.profile.cruds_count}Cruds</div>
           <div className="banner" style={styles} >
-            <div className='avatar'>
-              <img src='https://assets.pratiksinha.link/avatars/data.jpg'></img>
-            </div>
-          </div>
+          <ProfileAvatar id={props.profile.cognito_user_uuid} />
+    </div>
           
           <div className="info">
             <div className='id'>
